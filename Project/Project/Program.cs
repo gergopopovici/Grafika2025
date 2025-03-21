c62d0b960388f1f885ada94cad7874e6357224df
gergopopovici <popovicigergo0330@gmail.com>
2025-03-22 01:32:40 +0200

    Lab1-1 Completed.

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -10,18 +10,20 @@ namespace Project
         private static GL Gl;
         private static uint program;
 
+        // Issue 5: Corrupted shader string - missing semicolon
         private static readonly string VertexShaderSource = @"
         #version 330 core
         layout (location = 0) in vec3 vPos;
-        layout (location = 1) in vec4 vCol;
+        layout (location = 1) in vec4 vCol
         out vec4 outCol;
         void main()
         {
-            outCol = vCol
+            outCol = vCol;
             gl_Position = vec4(vPos.x, vPos.y, vPos.z, 1.0);
         }
         ";
 
+        // Issue 5: Corrupted shader string - invalid operation on vec4
         private static readonly string FragmentShaderSource = @"
         #version 330 core
         out vec4 FragColor;
@@ -35,7 +37,7 @@ namespace Project
         static void Main(string[] args)
         {
             WindowOptions windowOptions = WindowOptions.Default;
-            windowOptions.Title = "Lab1-1 Teszt";
+            windowOptions.Title = "Lab1-1 Háromszög";
             windowOptions.Size = new Silk.NET.Maths.Vector2D<int>(500, 500);
             graphicWindow = Window.Create(windowOptions);
             graphicWindow.Load += GraphicWindow_Load;
@@ -68,7 +70,7 @@ namespace Project
             Gl.GetShader(vshader, ShaderParameterName.CompileStatus, out int vStatus);
             if (vStatus != (int)GLEnum.True)
             {
-                Console.WriteLine($"Vertex shader compilation failed: {Gl.GetShaderInfoLog(vshader)}");
+                Console.WriteLine($"Vertex shader error: {Gl.GetShaderInfoLog(vshader)}");
             }
 
             Gl.ShaderSource(fshader, FragmentShaderSource);
@@ -78,14 +80,21 @@ namespace Project
             Gl.GetShader(fshader, ShaderParameterName.CompileStatus, out int fStatus);
             if (fStatus != (int)GLEnum.True)
             {
-                Console.WriteLine($"Fragment shader compilation failed: {Gl.GetShaderInfoLog(fshader)}");
+                Console.WriteLine($"Fragment shader error: {Gl.GetShaderInfoLog(fshader)}");
             }
 
+            // Issue 4: Incorrect order - LinkProgram called before AttachShader
             program = Gl.CreateProgram();
             Gl.LinkProgram(program);
             Gl.AttachShader(program, vshader);
             Gl.AttachShader(program, fshader);
             CheckGLError("Shader Program Linking");
+
+            Gl.GetProgram(program, GLEnum.LinkStatus, out int linkStatus);
+            if (linkStatus != (int)GLEnum.True)
+            {
+                Console.WriteLine($"Program linking error: {Gl.GetProgramInfoLog(program)}");
+            }
 
             Gl.DetachShader(program, vshader);
             Gl.DetachShader(program, fshader);
@@ -106,20 +115,23 @@ namespace Project
             uint vao = Gl.GenVertexArray();
             Gl.BindVertexArray(vao);
 
+            // Issue 1: Corrupted vertex array
             float[] vertexArray = new float[] {
                 -0.5f, -0.5f, 0.0f,
                 +0.5f, -0.5f, 0.0f,
                 0.0f, +0.5f, 0.0f,
-                999.0f
+                999.0f  // Invalid vertex coordinate (should be 3 values per vertex)
             };
 
             uint vertices = Gl.GenBuffer();
-            // Gl.BindBuffer(GLEnum.ArrayBuffer, vertices); 
+            // Issue 2: Missing BindBuffer call before BufferData
+            // Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
             Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
             CheckGLError("Uploading Vertex Buffer Data");
 
             Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(2);
+            // Issue 3: Wrong attribute index in EnableVertexAttribArray
+            Gl.EnableVertexAttribArray(2); // Should be 0 to match the layout in vertex shader
             CheckGLError("Vertex Attribute Setup");
 
             Gl.UseProgram(program);

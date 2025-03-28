dee58a9dfe9aa378949e7567bc59d019f2758b01
gergopopovici <popovicigergo0330@gmail.com>
2025-03-28 23:33:04 +0200

    Add initial implementation of OpenGL rendering in Program.cs

---
+++ Project/Project/Program.cs
@@ -0,0 +1,209 @@
+﻿using lab2_1;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using System;
+
+namespace Project
+{
+    internal static class Program
+    {
+
+        private static CameraDescriptor cameraDescriptor = new();
+
+        //private static CubeArrangementModel cubeArrangementModel = new();
+
+        private static IWindow window;
+
+        private static GL Gl;
+
+        private static uint[] vao = new uint[27];
+        private static uint vertices;
+        private static uint colors;
+        private static uint indices;
+        private static uint indexLength;
+
+        private static uint program;
+
+
+        private const string ModelMatrixVariableName = "uModel";
+        private const string ViewMatrixVariableName = "uView";
+        private const string ProjectionMatrixVariableName = "uProjection";
+
+        private static readonly string VertexShaderSource = @"
+        #version 330 core
+        layout (location = 0) in vec3 vPos;
+		layout (location = 1) in vec4 vCol;
+
+
+        uniform mat4 uModel;
+        uniform mat4 uView;
+        uniform mat4 uProjection;
+
+		out vec4 outCol;
+        
+        void main()
+        {
+			outCol = vCol;
+            gl_Position = uProjection*uView*uModel*vec4(vPos.x, vPos.y, vPos.z, 1.0);
+        }
+        ";
+
+
+        private static readonly string FragmentShaderSource = @"
+        #version 330 core
+        out vec4 FragColor;
+		
+		in vec4 outCol;
+
+        void main()
+        {
+            FragColor = outCol;
+        }
+        ";
+
+        static void Main(string[] args)
+        {
+            WindowOptions windowOptions = WindowOptions.Default;
+            windowOptions.Title = "lab2_1";
+            windowOptions.Size = new Vector2D<int>(500, 500);
+
+            window = Window.Create(windowOptions);
+
+            window.Load += Window_Load;
+            window.Update += Window_Update;
+            window.Render += Window_Render;
+            window.Closing += Window_Closing;
+
+            window.Run();
+        }
+
+        private static void GraphicWindow_Load()
+        {
+            // egszeri beallitasokat
+            //Console.WriteLine("Loaded");
+
+            Gl = graphicWindow.CreateOpenGL();
+
+            Gl.ClearColor(System.Drawing.Color.White);
+
+            uint vshader = Gl.CreateShader(ShaderType.VertexShader);
+            uint fshader = Gl.CreateShader(ShaderType.FragmentShader);
+
+            Gl.ShaderSource(vshader, VertexShaderSource);
+            Gl.CompileShader(vshader);
+            Gl.GetShader(vshader, ShaderParameterName.CompileStatus, out int vStatus);
+            if (vStatus != (int)GLEnum.True)
+                throw new Exception("Vertex shader failed to compile: " + Gl.GetShaderInfoLog(vshader));
+
+            Gl.ShaderSource(fshader, FragmentShaderSource);
+            Gl.CompileShader(fshader);
+
+            program = Gl.CreateProgram();
+            Gl.AttachShader(program, vshader);
+            Gl.AttachShader(program, fshader);
+            Gl.LinkProgram(program);
+            Gl.DetachShader(program, vshader);
+            Gl.DetachShader(program, fshader);
+            Gl.DeleteShader(vshader);
+            Gl.DeleteShader(fshader);
+
+            Gl.GetProgram(program, GLEnum.LinkStatus, out var status);
+            if (status == 0)
+            {
+                Console.WriteLine($"Error linking shader {Gl.GetProgramInfoLog(program)}");
+            }
+
+        }
+
+        private static void GraphicWindow_Update(double deltaTime)
+        {
+            // NO GL
+            // make it threadsave
+            //Console.WriteLine($"Update after {deltaTime} [s]");
+        }
+
+        private static unsafe void GraphicWindow_Render(double deltaTime)
+        {
+            //Console.WriteLine($"Render after {deltaTime} [s]");
+
+            Gl.Clear(ClearBufferMask.ColorBufferBit);
+
+            uint vao = Gl.GenVertexArray();
+            Gl.BindVertexArray(vao);
+
+            float[] vertexArray = new float[] {
+                -0.0f, -1.0f, 0.0f,     //0
+                -0.0f, 0.0f, 0.0f,      //1
+                -0.75f, -0.50f, 0.0f,   //2
+                -0.75f, +0.50f, 0.0f,   //3
+
+                -0.0f, -1.0f, 0.0f,     //4
+                -0.0f, 0.0f, 0.0f,      //5
+                +0.75f, -0.50f, 0.0f,   //6
+                +0.75f, +0.50f, 0.0f,   //7
+
+                0.0f, 0.0f, 0.0f,       //8
+                0.75f, 0.50f, 0.0f,     //9
+                -0.75f, 0.50f, 0.0f,    //10
+                0.0f, 1.0f, 0.0f,       //11
+            };
+
+            float[] colorArray = new float[] {
+                 1.0f, 0.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 0.0f, 1.0f,
+
+                0.0f, 0.0f, 1.0f, 1.0f,
+                0.0f, 0.0f, 1.0f, 0.0f,
+                0.0f, 0.0f, 1.0f, 1.0f,
+                0.0f, 0.0f, 1.0f, 0.0f,
+
+                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+            };
+
+            uint[] indexArray = new uint[] {
+                0, 1, 2,
+                1, 2, 3,
+
+                4, 5, 6,
+                5, 6, 7,
+
+               8, 9, 10,
+               9, 10, 11,
+            };
+
+            uint vertices = Gl.GenBuffer();
+            Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
+            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
+            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, null);
+            Gl.EnableVertexAttribArray(0);
+
+            uint colors = Gl.GenBuffer();
+            Gl.BindBuffer(GLEnum.ArrayBuffer, colors);
+            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
+            Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
+            Gl.EnableVertexAttribArray(1);
+
+            uint indices = Gl.GenBuffer();
+            Gl.BindBuffer(GLEnum.ElementArrayBuffer, indices);
+            Gl.BufferData(GLEnum.ElementArrayBuffer, (ReadOnlySpan<uint>)indexArray.AsSpan(), GLEnum.StaticDraw);
+            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
+            Gl.UseProgram(program);
+
+            Gl.DrawElements(GLEnum.Triangles, (uint)indexArray.Length, GLEnum.UnsignedInt, null); // we used element buffer
+            Gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
+            Gl.BindVertexArray(vao);
+
+            // always unbound the vertex buffer first, so no halfway results are displayed by accident
+            Gl.DeleteBuffer(vertices);
+            Gl.DeleteBuffer(colors);
+            Gl.DeleteBuffer(indices);
+            Gl.DeleteVertexArray(vao);
+        }
+    }
+}

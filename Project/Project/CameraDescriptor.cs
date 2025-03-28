3e04ad683c15d27c2d866e1fab9695ef79923753
gergopopovici <popovicigergo0330@gmail.com>
2025-03-28 23:30:38 +0200

    Starting Lab2 Assignments

---
+++ Project/Project/CameraDescriptor.cs
@@ -0,0 +1,108 @@
+﻿using Silk.NET.Maths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_1
+{
+    internal class CameraDescriptor
+    {
+        private double DistanceToOrigin = 1;
+
+        private double AngleToZYPlane = 0;
+
+        private double AngleToZXPlane = 0;
+
+        private const double DistanceScaleFactor = 1.1;
+
+        private const double AngleChangeStepSize = Math.PI / 180 * 5;
+
+        public Vector3D<float> Position
+        {
+            get
+            {
+                return GetPointFromAngles(DistanceToOrigin, AngleToZYPlane, AngleToZXPlane);
+            }
+        }
+
+        /// <summary>
+        /// Gets the up vector of the camera.
+        /// </summary>
+        public Vector3D<float> UpVector
+        {
+            get
+            {
+                return Vector3D.Normalize(GetPointFromAngles(DistanceToOrigin, AngleToZYPlane, AngleToZXPlane + Math.PI / 2));
+            }
+        }
+
+        /// <summary>
+        /// Gets the target point of the camera view.
+        /// </summary>
+        public Vector3D<float> Target
+        {
+            get
+            {
+                // For the moment the camera is always pointed at the origin.
+                return Vector3D<float>.Zero;
+            }
+        }
+
+        public void IncreaseZXAngle()
+        {
+            AngleToZXPlane += AngleChangeStepSize;
+        }
+
+        public void DecreaseZXAngle()
+        {
+            AngleToZXPlane -= AngleChangeStepSize;
+        }
+
+        public void SetZXAngle(float angle)
+        { 
+            AngleToZXPlane = angle; 
+        }
+
+        public void IncreaseZYAngle()
+        {
+            AngleToZYPlane += AngleChangeStepSize;
+
+        }
+
+        public void DecreaseZYAngle()
+        {
+            AngleToZYPlane -= AngleChangeStepSize;
+        }
+
+        public void SetZYAngle(float angle)
+        {
+            AngleToZYPlane += angle;
+        }
+
+        public void IncreaseDistance()
+        {
+            DistanceToOrigin = DistanceToOrigin * DistanceScaleFactor;
+        }
+
+        public void DecreaseDistance()
+        {
+            DistanceToOrigin = DistanceToOrigin / DistanceScaleFactor;
+        }
+
+        public void SetDistance(float distance)
+        {
+            DistanceToOrigin = distance;
+        }
+
+        private static Vector3D<float> GetPointFromAngles(double distanceToOrigin, double angleToMinZYPlane, double angleToMinZXPlane)
+        {
+            var x = distanceToOrigin * Math.Sin(angleToMinZYPlane) * Math.Cos(angleToMinZXPlane);
+            var z = distanceToOrigin * Math.Cos(angleToMinZYPlane) * Math.Cos(angleToMinZXPlane);
+            var y = distanceToOrigin * Math.Sin(angleToMinZXPlane);
+
+            return new Vector3D<float>((float)x, (float)y, (float)z);
+        }
+    }
+}

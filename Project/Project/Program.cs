c50115dfe03672b4d38f742ab8ebe6e3e2921bae
gergopopovici <popovicigergo0330@gmail.com>
2025-03-29 00:08:22 +0200

    Implement 3D Rubik's Cube rendering with OpenGL

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -77,16 +77,37 @@ namespace Project
 
             window.Run();
         }
-
-        private static void GraphicWindow_Load()
+        public static void CheckError(String func_name)
         {
-            // egszeri beallitasokat
-            //Console.WriteLine("Loaded");
+            var error = (ErrorCode)Gl.GetError();
+            if (error != ErrorCode.NoError)
+                throw new Exception("Error(" + func_name + ") GL.GetError() returned " + error.ToString());
+        }
 
-            Gl = graphicWindow.CreateOpenGL();
+        private static void Window_Load()
+        {
+            //Console.WriteLine("Load");
+
+            cameraDescriptor.SetDistance(4.56f);
+            cameraDescriptor.SetZXAngle((float)Math.PI / 180 * 30);
+            cameraDescriptor.SetZYAngle((float)Math.PI / 180 * 30);
+
+            Gl = window.CreateOpenGL();
 
             Gl.ClearColor(System.Drawing.Color.White);
 
+            SetUpModelObjects();
+
+            CreateProgram();
+
+            Gl.Enable(EnableCap.CullFace);
+
+            Gl.Enable(EnableCap.DepthTest);
+            Gl.DepthFunc(DepthFunction.Lequal);
+        }
+
+        private static void CreateProgram()
+        {
             uint vshader = Gl.CreateShader(ShaderType.VertexShader);
             uint fshader = Gl.CreateShader(ShaderType.FragmentShader);
 
@@ -103,107 +124,272 @@ namespace Project
             Gl.AttachShader(program, vshader);
             Gl.AttachShader(program, fshader);
             Gl.LinkProgram(program);
-            Gl.DetachShader(program, vshader);
-            Gl.DetachShader(program, fshader);
-            Gl.DeleteShader(vshader);
-            Gl.DeleteShader(fshader);
-
             Gl.GetProgram(program, GLEnum.LinkStatus, out var status);
             if (status == 0)
             {
                 Console.WriteLine($"Error linking shader {Gl.GetProgramInfoLog(program)}");
             }
-
+            Gl.DetachShader(program, vshader);
+            Gl.DetachShader(program, fshader);
+            Gl.DeleteShader(vshader);
+            Gl.DeleteShader(fshader);
         }
 
-        private static void GraphicWindow_Update(double deltaTime)
+
+        private static void Window_Update(double deltaTime)
         {
-            // NO GL
-            // make it threadsave
-            //Console.WriteLine($"Update after {deltaTime} [s]");
+            //Console.WriteLine($"Update after {deltaTime} [s].");
+            // multithreaded
+            // make sure it is threadsafe
+            // NO GL calls
+            //cubeArrangementModel.AdvanceTime(deltaTime);
         }
 
-        private static unsafe void GraphicWindow_Render(double deltaTime)
+        private static unsafe void SetModelMatrix(Matrix4X4<float> modelMatrix)
         {
-            //Console.WriteLine($"Render after {deltaTime} [s]");
 
-            Gl.Clear(ClearBufferMask.ColorBufferBit);
+            int location = Gl.GetUniformLocation(program, ModelMatrixVariableName);
+            if (location == -1)
+            {
+                throw new Exception($"{ModelMatrixVariableName} uniform not found on shader.");
+            }
 
-            uint vao = Gl.GenVertexArray();
-            Gl.BindVertexArray(vao);
+            Gl.UniformMatrix4(location, 1, false, (float*)&modelMatrix);
+            CheckError("SetModelMatrix");
+        }
 
+        private static unsafe void SetUpModelObjects()
+        {
+
+            // counter clockwise is front facing
             float[] vertexArray = new float[] {
-                -0.0f, -1.0f, 0.0f,     //0
-                -0.0f, 0.0f, 0.0f,      //1
-                -0.75f, -0.50f, 0.0f,   //2
-                -0.75f, +0.50f, 0.0f,   //3
+                -0.5f, 0.5f, 0.5f,
+                0.5f, 0.5f, 0.5f,
+                0.5f, 0.5f, -0.5f,
+                -0.5f, 0.5f, -0.5f,
 
-                -0.0f, -1.0f, 0.0f,     //4
-                -0.0f, 0.0f, 0.0f,      //5
-                +0.75f, -0.50f, 0.0f,   //6
-                +0.75f, +0.50f, 0.0f,   //7
+                -0.5f, 0.5f, 0.5f,
+                -0.5f, -0.5f, 0.5f,
+                0.5f, -0.5f, 0.5f,
+                0.5f, 0.5f, 0.5f,
 
-                0.0f, 0.0f, 0.0f,       //8
-                0.75f, 0.50f, 0.0f,     //9
-                -0.75f, 0.50f, 0.0f,    //10
-                0.0f, 1.0f, 0.0f,       //11
+                -0.5f, 0.5f, 0.5f,
+                -0.5f, 0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+                -0.5f, -0.5f, 0.5f,
+
+                -0.5f, -0.5f, 0.5f,
+                0.5f, -0.5f, 0.5f,
+                0.5f, -0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+
+                0.5f, 0.5f, -0.5f,
+                -0.5f, 0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, -0.5f,
+
+                0.5f, 0.5f, 0.5f,
+                0.5f, 0.5f, -0.5f,
+                0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, 0.5f,
+
             };
 
+            /*
             float[] colorArray = new float[] {
-                 1.0f, 0.0f, 0.0f, 1.0f,
+                //top
+                1.0f, 0.0f, 0.0f, 1.0f,
                 1.0f, 0.0f, 0.0f, 1.0f,
                 1.0f, 0.0f, 0.0f, 1.0f,
                 1.0f, 0.0f, 0.0f, 1.0f,
 
-                0.0f, 0.0f, 1.0f, 1.0f,
-                0.0f, 0.0f, 1.0f, 0.0f,
-                0.0f, 0.0f, 1.0f, 1.0f,
-                0.0f, 0.0f, 1.0f, 0.0f,
+                //front
+                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
 
-                0.0f, 1.0f, 0.0f, 1.0f,
-                0.0f, 1.0f, 0.0f, 1.0f,
-                0.0f, 1.0f, 0.0f, 1.0f,
-                0.0f, 1.0f, 0.0f, 1.0f,
-            };
+                //left
+                0.0f, 0.0f, 1.0f, 1.0f,
+                0.0f, 0.0f, 1.0f, 1.0f,
+                0.0f, 0.0f, 1.0f, 1.0f,
+                0.0f, 0.0f, 1.0f, 1.0f,
+
+                //bottom
+                1.0f, 0.0f, 1.0f, 1.0f,
+                1.0f, 0.0f, 1.0f, 1.0f,
+                1.0f, 0.0f, 1.0f, 1.0f,
+                1.0f, 0.0f, 1.0f, 1.0f,
+
+                //back
+                0.0f, 1.0f, 1.0f, 1.0f,
+                0.0f, 1.0f, 1.0f, 1.0f,
+                0.0f, 1.0f, 1.0f, 1.0f,
+                0.0f, 1.0f, 1.0f, 1.0f,
+
+                //right
+                1.0f, 1.0f, 0.0f, 1.0f,
+                1.0f, 1.0f, 0.0f, 1.0f,
+                1.0f, 1.0f, 0.0f, 1.0f,
+                1.0f, 1.0f, 0.0f, 1.0f,
+            };*/
+
+
+            float[][] colorArray = new float[27][];
+            for (int i = 0; i < 27; i++)
+            {
+                colorArray[i] = new float[96];
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    for (int k = 0; k < 3; ++k)
+                    {
+                        for (int l = 0; l < 96; l += 4)
+                        {
+                            colorArray[i * 9 + j * 3 + k][l + 3] = 1.0f;
+                            if (l < 16)
+                            {
+                                if (i > 1)
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 1.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 0.0f;
+                                }
+                                else
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 0.0f;
+                                }
+                            }
+                            else if (l < 32)
+                            {
+                                if (j > 1)
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 1.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 0.0f;
+                                }
+                                else
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 0.0f;
+                                }
+                            }
+                            else if (l < 48)
+                            {
+                                if (k < 1)
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 1.0f;
+                                }
+                                else
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 0.0f;
+                                }
+                            }
+                            else if (l < 64)
+                            {
+                                if (i < 1)
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 1.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 1.0f;
+                                }
+                                else
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 0.0f;
+                                }
+                            }
+                            else if (l < 80)
+                            {
+                                if (j < 1)
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 1.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 1.0f;
+                                }
+                                else
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 0.0f;
+                                }
+                            }
+                            else if (l < 96)
+                            {
+                                if (k > 1)
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 1.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 1.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 0.0f;
+                                }
+                                else
+                                {
+                                    colorArray[i * 9 + j * 3 + k][l] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 1] = 0.0f;
+                                    colorArray[i * 9 + j * 3 + k][l + 2] = 0.0f;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
 
             uint[] indexArray = new uint[] {
                 0, 1, 2,
-                1, 2, 3,
+                0, 2, 3,
 
                 4, 5, 6,
-                5, 6, 7,
+                4, 6, 7,
 
-               8, 9, 10,
-               9, 10, 11,
+                8, 9, 10,
+                10, 11, 8,
+
+                12, 14, 13,
+                12, 15, 14,
+
+                17, 16, 19,
+                17, 19, 18,
+
+                20, 22, 21,
+                20, 23, 22
             };
 
-            uint vertices = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
-            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
-            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(0);
+            for (int i = 0; i < 27; i++)
+            {
+                vao[i] = Gl.GenVertexArray();
+                Gl.BindVertexArray(vao[i]);
+                vertices = Gl.GenBuffer();
+                Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
+                Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
+                Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, null);
+                Gl.EnableVertexAttribArray(0);
 
-            uint colors = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ArrayBuffer, colors);
-            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
-            Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(1);
+                colors = Gl.GenBuffer();
+                Gl.BindBuffer(GLEnum.ArrayBuffer, colors);
 
-            uint indices = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ElementArrayBuffer, indices);
-            Gl.BufferData(GLEnum.ElementArrayBuffer, (ReadOnlySpan<uint>)indexArray.AsSpan(), GLEnum.StaticDraw);
-            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
-            Gl.UseProgram(program);
+                Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray[i].AsSpan(), GLEnum.StaticDraw);
+                Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
+                Gl.EnableVertexAttribArray(1);
 
-            Gl.DrawElements(GLEnum.Triangles, (uint)indexArray.Length, GLEnum.UnsignedInt, null); // we used element buffer
-            Gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
-            Gl.BindVertexArray(vao);
+                indices = Gl.GenBuffer();
+                Gl.BindBuffer(GLEnum.ElementArrayBuffer, indices);
+                Gl.BufferData(GLEnum.ElementArrayBuffer, (ReadOnlySpan<uint>)indexArray.AsSpan(), GLEnum.StaticDraw);
+            }
 
-            // always unbound the vertex buffer first, so no halfway results are displayed by accident
-            Gl.DeleteBuffer(vertices);
-            Gl.DeleteBuffer(colors);
-            Gl.DeleteBuffer(indices);
-            Gl.DeleteVertexArray(vao);
+            Gl.BindVertexArray(0);
+
+            indexLength = (uint)indexArray.Length;
         }
     }
 }
